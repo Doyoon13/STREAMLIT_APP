1a8f235f92a513489673cd3f360c40eb3aef207f
Doyoon13 <123908960+Doyoon13@users.noreply.github.com>
2024-07-18 11:11:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import sqlite3
 
 # Imports the functions we coded above
 from response import *
@@ -15,3 +16,13 @@ if st.button('Start Timer'):
 title_placeholder = st.empty()
 
 Timer_placeholder = st.empty()
+
+@st.cache
+def count_visitor():
+conn = sqlite3.connect("/path/to/database.db")
+cur = conn.cursor()
+cur.execute("UPDATE name_of_table SET total_visitors = total_visitors + " + str(1))
+conn.commit()
+conn.close()
+
+count_visitor()
