dcc1ed72f9d7f3d8fb3c59ca97b37b2d7cb0abac
Doyoon13 <123908960+Doyoon13@users.noreply.github.com>
2024-07-18 16:22:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,17 +7,25 @@ from header import *
 # Create the header
 create_header()
 
+if 'remaining_time' not in st.session_state:
+  st.session_state['remaining_time'] = 0
+
 days = st.number_input('Days:')
 hours = st.number_input('Hours:')
 minutes = st.number_input('Minutes:')
 
-if st.button('Start Timer'):
-    countdown(days, hours, minutes)
-if st.button('Reset Timer'):
-    countdown(0,0,0)
-    timer_placeholder.title("Timer Reseted")
+col1, col2 = st.columns(2)
 
+with col1:
+  if st.button('Start Timer'):
+    st.session_state['remaining_time'] = days * 86400 + hours * 3600 + minutes * 60
+    
+with col2:
+  if st.button('Reset Timer'):
+    st.session_state['remaining_time'] = 0
 
+if st.session_state['remaining_time'] > 0:
+  countdown(0, 0, st.session_state['remaining_time'])
 
 title_placeholder = st.empty()
 
