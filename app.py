4b5fef9814dd3dc24aeaeb9105246cfe2dc9fd31
Doyoon13 <123908960+Doyoon13@users.noreply.github.com>
2024-07-18 16:20:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,13 @@ create_header()
 days = st.number_input('Days:')
 hours = st.number_input('Hours:')
 minutes = st.number_input('Minutes:')
-if st.button('Reset Timer'):
-    countdown(0,0,0)
-    
+
 if st.button('Start Timer'):
     countdown(days, hours, minutes)
+if st.button('Reset Timer'):
+    countdown(0,0,0)
+    timer_placeholder.title("Timer Reseted")
+
 
 
 title_placeholder = st.empty()
