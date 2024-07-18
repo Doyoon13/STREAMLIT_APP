10c605c6a44490fce84e861506f0f9d76ab84eaa
Doyoon13 <123908960+Doyoon13@users.noreply.github.com>
2024-07-18 11:13:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,12 +17,4 @@ title_placeholder = st.empty()
 
 Timer_placeholder = st.empty()
 
-@st.cache
-def count_visitor():
- conn = sqlite3.connect("/path/to/database.db")
- cur = conn.cursor()
- cur.execute("UPDATE name_of_table SET total_visitors = total_visitors + " + str(1))
- conn.commit()
- conn.close()
 
-count_visitor()
