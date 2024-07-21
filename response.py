9b97bdb2e253130d97e7fe1f14c9fdc9a2905599
Doyoon13 <123908960+Doyoon13@users.noreply.github.com>
2024-07-21 14:36:20 +0900

    Update response.py

--- response.py
+++ response.py
@@ -11,7 +11,7 @@ def countdown(duration_days, duration_hours, duration_minutes):
   timer_placeholder = st.empty()
 
   while time.time() <= end_time:
-    remaining_time = int(end_time - time.time())
+    remaining_time = int(end_time - (time.time()+1))
     days, remainder1 = divmod(remaining_time, 86400)
     hours, remainder2 = divmod(remainder1, 3600)
     mins, secs = divmod(remainder2, 60)
