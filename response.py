3588507223b8da09d0aa40f70265de86e3324f94
Doyoon13 <123908960+Doyoon13@users.noreply.github.com>
2024-07-21 16:30:43 +0900

    Update response.py

--- response.py
+++ response.py
@@ -1,12 +1,17 @@
+%%writefile response.py
 import time
 import streamlit as st
 
 
 def countdown(duration_days, duration_hours, duration_minutes):
- total_seconds = duration_days * 86400 + duration_hours * 3600 + duration_minutes * 60
- start_time = time.time()
- end_time = start_time + total_seconds
- while time.time() < end_time:
+
+  total_seconds = duration_days * 86400 + duration_hours * 3600 + duration_minutes * 60
+  start_time = time.time()
+  end_time = start_time + total_seconds
+
+  timer_placeholder = st.empty()
+
+  while time.time() < end_time:
     remaining_time = int(end_time - time.time())
     days, remainder1 = divmod(remaining_time, 86400)
     hours, remainder2 = divmod(remainder1, 3600)
@@ -14,12 +19,11 @@ def countdown(duration_days, duration_hours, duration_minutes):
     timer = '{:02d}:{:02d}:{:02d}:{:02d}'.format(days,hours, mins, secs)
 
     timer_placeholder.title(timer)
-    time.sleep(0)
+
     time.sleep(1)
 
-timer_placeholder = st.empty()  
-timer_placeholder.title("Time's up!")
-audio_path = 'mixkit-vintage-warning-alarm-990.wav'
-st.audio(audio_path, autoplay = True)
+  audio_path = 'mixkit-vintage-warning-alarm-990.wav'
+  st.audio(audio_path, autoplay = True)
+  timer_placeholder.title("Time's up!")
 
 
